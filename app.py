571594f48ab7b488d84deb0b5d44d8dacd1731c8
Eric Liu <ericshen.18888@gmail.com>
2025-10-15 14:24:06 +0800

    Added POST endpoint and fixed long endpoint timeout

--- app.py
+++ app.py
@@ -1,10 +1,29 @@
-from flask import Flask
+from flask import Flask, request
+import time
 
 app = Flask(__name__)
 
 @app.route('/hello_world')
 def hello_world():
     return {'message': 'Hello, World!'}
+
+
+
+@app.route('/hello_world/<name>')
+def hello_world_name(name):
+    return {'message': 'Hello, ' + name + ' World!'}
+
+
+@app.route('/hello_world/long')
+def hello_world_long():
+    time.sleep(5)
+    return {'message': 'Hello, World!'}
+
+@app.route('/hello', methods=['POST'])
+def hello():
+    data = request.get_json()
+    name = data.get('name', 'Anonymous')
+    return {'message': f'Hello, {name}! This is a POST response.'}
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=8080)
